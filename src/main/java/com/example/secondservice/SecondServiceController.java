d6a2b4290291b912d73029ea9085c4ce3021c144
taehyun9244 <namtaehyun9244@gmail.com>
2022-06-25 22:42:50 +0900

    discoveryService and apiGateway connection

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,16 +1,25 @@
 package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/second-service")
 @Slf4j
 public class SecondServiceController {
+    Environment env;
 
+    @Autowired
+    public SecondServiceController(Environment env){
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
@@ -23,7 +32,9 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "this is second service check";
+    public String check(HttpServletRequest request){
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("this is first service check %s,", env.getProperty("local.server.port"));
     }
 }
